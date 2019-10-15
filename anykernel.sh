9f43ce5c84f8138b4a7e840cfff1ec6917ec5e9d
Zachariah Kennedy <zkennedy87@gmail.com>
2019-10-15 17:59:50 +0000

    AK3: initial RenderZenith zip files
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -35,16 +35,39 @@ set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 
+## begin vendor changes
+#mount -o rw,remount -t auto /vendor >/dev/null;
+
+# Make a backup of init.target.rc
+#restore_file /vendor/etc/init/hw/init.target.rc;
+#backup_file /vendor/etc/init/hw/init.target.rc;
+
+# Do work
+#replace_string /vendor/etc/init/hw/init.target.rc "write /dev/stune/top-app/schedtune.colocate 0" "write /dev/stune/top-app/schedtune.colocate 1" "write /dev/stune/top-app/schedtune.colocate 0";
+
+# Add RZ tweaks
+#append_file /vendor/etc/init/hw/init.target.rc "R4ND0MSTR1NG" init.target.rc;
+
+
 ## AnyKernel install
 dump_boot;
+
+# Clean up other kernels' ramdisk overlay files
+rm -rf $ramdisk/overlay;
+rm -rf $ramdisk/overlay.d;
 
 # begin ramdisk changes
 # Add skip_override parameter to cmdline so user doesn't have to reflash Magisk
 if [ -d $ramdisk/.backup ]; then
-  ui_print " "; ui_print "Magisk detected! Patching cmdline so reflashing Magisk is not necessary...";
-  patch_cmdline "skip_override" "skip_override";
+	ui_print " "; ui_print "Magisk detected! Patching cmdline so reflashing Magisk is not necessary...";
+	patch_cmdline "skip_override" "skip_override";
+	mv /tmp/anykernel/overlay.d $ramdisk/overlay.d
+	chmod -R 750 $ramdisk/overlay.d/*
+	chown -R root:root $ramdisk/overlay.d/*
+	chmod -R 755 $ramdisk/overlay.d/sbin/init.renderzenith.sh
+	chown -R root:root $ramdisk/overlay.d/sbin/init.renderzenith.sh
 else
-  patch_cmdline "skip_override" "";
+	patch_cmdline "skip_override" "";
 fi;
 
 # end ramdisk changes
