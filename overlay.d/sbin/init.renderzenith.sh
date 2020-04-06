b8ceb6a3779d49f7ffa17380364920b2daa2ed06
Zachariah Kennedy <zkennedy87@gmail.com>
2020-04-06 13:48:15 +0000

    RZ: Optimize UX task's cgroup membership for our task placement algorithm
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>

--- overlay.d/sbin/init.renderzenith.sh
+++ overlay.d/sbin/init.renderzenith.sh
@@ -75,4 +75,40 @@ sleep 15;
 # Disable OP Service related to OPChain and the rest
 	resetprop ctl.stop oneplus_brain_service
 
+
+# EXPERIMENTAL: Optimize UX task cgroup membership
+	PIDSS=`ps -AT | grep system_server | awk '{print $3}'`
+
+	echo $PIDSS > /dev/cpuset/foreground/cgroup.procs
+	echo $PIDSS > /dev/stune/foreground/cgroup.procs
+
+	PIDAIO=`ps -AT | grep android.io | awk '{print $3}'`
+
+	echo $PIDAIO > /dev/stune/foreground/tasks
+
+	PIDAA=`ps -AT | grep android.anim | awk '{print $3}'`
+
+	echo $PIDAA > /dev/cpuset/top-app/tasks
+
+	PIDAALF=`ps -AT | grep android.anim.lf | awk '{print $3}'`
+
+	echo $PIDAALF > /dev/cpuset/top-app/tasks
+
+	PIDAFG=`ps -AT | grep android.fg | awk '{print $3}'`
+
+	echo $PIDAFG > /dev/stune/foreground/tasks
+
+	PIDAUI=`ps -AT | grep android.ui | awk '{print $3}'`
+
+	echo $PIDAUI > /dev/stune/top-app/tasks
+
+	PIDAD=`ps -AT | grep android.display | awk '{print $3}'`
+
+	echo $PIDAD > /dev/cpuset/top-app/tasks
+	echo $PIDAD > /dev/stune/top-app/tasks
+
+	PIDRECLAIMD=`ps -AT | grep reclaimd | awk '{print $3}'`
+
+	echo $PIDRECLAIMD > /dev/stune/top-app/tasks
+
 echo "RenderZenith Boot Completed" >> /dev/kmsg
