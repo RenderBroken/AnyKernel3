5e7fc58c36de9afa6da66f59ab8143714fed37fd
Zachariah Kennedy <zkennedy87@gmail.com>
2020-04-16 20:52:28 +0000

    RZ: Testing
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>

--- overlay.d/sbin/init.renderzenith.sh
+++ overlay.d/sbin/init.renderzenith.sh
@@ -51,12 +51,12 @@ sleep 30;
 # Setup EAS cpusets values for better load balancing
 	echo 0-7 > /dev/cpuset/top-app/cpus
 	echo 0-3,6-7 > /dev/cpuset/foreground/cpus
-	echo 0-1 > /dev/cpuset/background/cpus
+	echo 0-3 > /dev/cpuset/background/cpus
 	echo 0-3 > /dev/cpuset/system-background/cpus
 	echo 0-7 > /dev/cpuset/display/cpus
 
 # For better screen off idle
-	echo 0-3 > /dev/cpuset/restricted/cpus
+#	echo 0-3 > /dev/cpuset/restricted/cpus
 
 # Tune FS
 	echo 3000 > /proc/sys/vm/dirty_expire_centisecs
