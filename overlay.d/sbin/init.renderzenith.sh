b3943d2070c12e96b66c79e1943cf2172fdfe058
Zachariah Kennedy <zkennedy87@gmail.com>
2020-06-04 22:30:14 +0000

    STASH
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>

--- overlay.d/sbin/init.renderzenith.sh
+++ overlay.d/sbin/init.renderzenith.sh
@@ -5,16 +5,16 @@ sleep 30;
 # Applying RenderZenith Settings
 
 # Disable sched_boost during CONSERVATIVE_BOOST
-	echo 0 > /dev/stune/foreground/schedtune.sched_boost_no_override
-	echo 0 > /dev/stune/top-app/schedtune.sched_boost_no_override
+#	echo 0 > /dev/stune/foreground/schedtune.sched_boost_no_override
+#	echo 0 > /dev/stune/top-app/schedtune.sched_boost_no_override
 
 # Tune Core_CTL for proper task placement
-	echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/enable
-	echo 0 > /sys/devices/system/cpu/cpu4/core_ctl/enable
-	echo 0 > /sys/devices/system/cpu/cpu7/core_ctl/enable
+#	echo "50" > /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres
 
-# Disable CAF task placement for Big Cores
-	echo 0 > /proc/sys/kernel/sched_walt_rotate_big_tasks
+# Tune for WALT
+	echo 15 > /proc/sys/kernel/sched_group_downmigrate
+	echo 400000000 > /proc/sys/kernel/sched_coloc_downmigrate_ns
+	echo 30 > /proc/sys/kernel/sched_min_task_util_for_colocation
 
 # Disable WALT Signal and use PELT
 #	echo 1 > /proc/sys/kernel/sched_use_walt_cpu_util
@@ -28,48 +28,48 @@ sleep 30;
 # Input boost and stune configuration
 	echo "0:1382400" > /sys/module/cpu_boost/parameters/input_boost_freq
 	echo 500 > /sys/module/cpu_boost/parameters/input_boost_ms
-	echo 15 > /sys/module/cpu_boost/parameters/dynamic_stune_boost
-	echo 1500 > /sys/module/cpu_boost/parameters/dynamic_stune_boost_ms
+#	echo 15 > /sys/module/cpu_boost/parameters/dynamic_stune_boost
+#	echo 1500 > /sys/module/cpu_boost/parameters/dynamic_stune_boost_ms
 
 # Dynamic Stune Boost during sched_boost
-	echo 15 > /dev/stune/top-app/schedtune.sched_boost
+#	echo 15 > /dev/stune/top-app/schedtune.sched_boost
 
 # Set min cpu freq
 	echo 576000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 	echo 710400 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
 	echo 825600 > /sys/devices/system/cpu/cpu7/cpufreq/scaling_min_freq
 
-# Setup Schedutil Governor
-	echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-	echo 500 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/up_rate_limit_us
-	echo 20000 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us
-	echo 1 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/iowait_boost_enable
-	echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/pl
-	echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/hispeed_freq
+# configure governor settings for silver cluster
+	echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+	echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/up_rate_limit_us
+	echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us
+	echo 1209600 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/hispeed_freq
+	echo 576000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
+	echo 1 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/pl
 
-	echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor	
-	echo 500 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/up_rate_limit_us
-	echo 20000 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/down_rate_limit_us
-	echo 1 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/iowait_boost_enable
-	echo 0 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/pl
-	echo 0 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/hispeed_freq
+# configure governor settings for gold cluster
+	echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy4/scaling_governor
+	echo 0 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/up_rate_limit_us
+	echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us
+	echo 1612800 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/hispeed_freq
+	echo 1 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/pl
 
-	echo "schedutil" > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor	
-	echo 500 > /sys/devices/system/cpu/cpufreq/policy7/schedutil/up_rate_limit_us
-	echo 20000 > /sys/devices/system/cpu/cpufreq/policy7/schedutil/down_rate_limit_us
-	echo 1 > /sys/devices/system/cpu/cpufreq/policy7/schedutil/iowait_boost_enable
-	echo 0 > /sys/devices/system/cpu/cpufreq/policy7/schedutil/pl
-	echo 0 > /sys/devices/system/cpu/cpufreq/policy7/schedutil/hispeed_freq
+# configure governor settings for gold+ cluster
+	echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy7/scaling_governor
+	echo 0 > /sys/devices/system/cpu/cpufreq/policy7/schedutil/up_rate_limit_us
+	echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us
+	echo 1612800 > /sys/devices/system/cpu/cpufreq/policy7/schedutil/hispeed_freq
+	echo 1 > /sys/devices/system/cpu/cpufreq/policy7/schedutil/pl
 
 # Setup EAS cpusets values for better load balancing
 	echo 0-7 > /dev/cpuset/top-app/cpus
-	echo 0-3,5-6 > /dev/cpuset/foreground/cpus
+	echo 0-7 > /dev/cpuset/foreground/cpus
 	echo 0-3 > /dev/cpuset/background/cpus
 	echo 0-3 > /dev/cpuset/system-background/cpus
 	echo 0-7 > /dev/cpuset/display/cpus
 
 # For better screen off idle
-#	echo 0-3 > /dev/cpuset/restricted/cpus
+	echo 0-3 > /dev/cpuset/restricted/cpus
 
 # Tune FS
 	echo 3000 > /proc/sys/vm/dirty_expire_centisecs
@@ -86,41 +86,5 @@ sleep 15;
 
 # Disable OP Service related to OPChain and the rest
 	resetprop ctl.stop oneplus_brain_service
-
-
-# EXPERIMENTAL: Optimize UX task cgroup membership
-	PIDSS=`ps -AT | grep system_server | awk '{print $3}'`
-
-	echo $PIDSS > /dev/cpuset/foreground/cgroup.procs
-	echo $PIDSS > /dev/stune/foreground/cgroup.procs
-
-	PIDAIO=`ps -AT | grep android.io | awk '{print $3}'`
-
-	echo $PIDAIO > /dev/stune/foreground/tasks
-
-	PIDAA=`ps -AT | grep android.anim | awk '{print $3}'`
-
-	echo $PIDAA > /dev/cpuset/top-app/tasks
-
-	PIDAALF=`ps -AT | grep android.anim.lf | awk '{print $3}'`
-
-	echo $PIDAALF > /dev/cpuset/top-app/tasks
-
-	PIDAFG=`ps -AT | grep android.fg | awk '{print $3}'`
-
-	echo $PIDAFG > /dev/stune/foreground/tasks
-
-	PIDAUI=`ps -AT | grep android.ui | awk '{print $3}'`
-
-	echo $PIDAUI > /dev/stune/top-app/tasks
-
-	PIDAD=`ps -AT | grep android.display | awk '{print $3}'`
-
-	echo $PIDAD > /dev/cpuset/top-app/tasks
-	echo $PIDAD > /dev/stune/top-app/tasks
-
-	PIDRECLAIMD=`ps -AT | grep reclaimd | awk '{print $3}'`
-
-	echo $PIDRECLAIMD > /dev/stune/top-app/tasks
 
 echo "RenderZenith Boot Completed" >> /dev/kmsg
